118397e81975f04786e34728b15d3d843e8340b4
jillian131511 <jilliana.2024@computing.smu.edu.sg>
2025-10-06 16:30:03 +0800

    Test

--- dunk/src/components/FileUpload.vue
+++ dunk/src/components/FileUpload.vue
@@ -1,9 +1,32 @@
 <script setup>
+import { ref as storageRef, uploadBytes, getDownloadURL } from 'firebase/storage' // Google's Firebase library
+import { storage } from '../firebase/firebase' // your local firebase setup
 </script>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  methods: {
+    async upload(event) {
+      // Get the selected file
+      const file = event.target.files[0]
+      // Create a reference to the file in Firebase Storage
+      const fileRef = storageRef(storage, `uploads/${file.name}`)
+      // Upload the file
+      await uploadBytes(fileRef, file)
+      // Get the URL to download the file
+      const url = await getDownloadURL(fileRef)
+      alert(`File uploaded: ${url}`)
+    }
+  }
+}
+</script>
+
 <template>
-    <RouterView>
-        
-    </RouterView>
+  <div>
+    <h1>Welcome to Vue + Firebase</h1>
+    <input type="file" @change="upload" />
+  </div>
 </template>
-<style scoped>
-</style>
--- dunk/src/components/Home.vue
+++ dunk/src/components/Home.vue
@@ -1,4 +1,34 @@
-<script> </script>
- <template> 
- <h1>Home</h1> 
+<template>
+  <div>
+    <h1>Welcome to Vue + Firebase</h1>
+    <button @click="login">Login with Google</button>
+    <h2 v-if="uploadOK">
+      <router-link to="/upload/">Upload</router-link>
+    </h2>
+  </div>
 </template>
+
+<script setup>
+import { loginWithGoogle } from '../firebase/auth'
+</script>
+
+<script>
+export default {
+  data() {
+    return {
+      uploadOK: false
+    }
+  },
+  methods: {
+    async login() {
+      try {
+        await loginWithGoogle()
+        alert('Logged in!')
+        this.uploadOK = true
+      } catch (err) {
+        console.error(err)
+      }
+    }
+  }
+}
+</script>
